6e15d45cd2aff11b9cced59617b2cf50b5245c82
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-11-08 21:25:33 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-int S=12;
-int p;
-cout<<"Dame un numero_"<<endl;
-  cin>>p;
-  if(p%2==0)
-    p=p+1;
-    for(int i=2; i<=(p/2);i++)
-    if(p%i==0)
-    p=p+2;
-    if(p<S)
-    S=S-p;
-    else
-    S=S-1;
-    if(S=0)
-    cout<<"tu numero final es"<<p<<endl;
-    else
-    p=p+2;
-    cout<<"tu numero es"<<p<<endl;
-    return 0;
+    int x;
+    cout<<"introduce un numero_"<<endl;
+    cin>>x;
+    if(x<=5)
+    {
+        if(x%3==0)
+        {
+            x=x+2;
+            cout<<"tu numero es_"<<x<<endl;
+        }
+        else
+        {
+            cout<<"tu numero es_"<<x<<endl;
+        }
+    }
+
+return 0;
 }
-
-
 
