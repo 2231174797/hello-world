2acfd820f4fbaf7f5561696a35e7fd4128a1880f
2231174797 <31254610+2231174797@users.noreply.github.com>
2017-12-04 10:42:33 -0600

    Add files via upload
    integral

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include<iostream>
+#include<math.h>
+#include<fstream>
 using namespace std;
-int main()
+double f1x(double x){
+  return -sin(x);
+}
+double fx(double x){
+  return cos(x);
+}
+int main(){
+    double b=0;
+    double a=0;
+    double n=0;
+    double sum=0;
+    cout<<"Dame el limite superior_"<<b<<endl;
+    cin>>b;
+    cout<<"Dame el limite inferior_"<<a<<endl;
+    cin>>a;
+    cout<<"Dame el numero de particiones_"<<n<<endl;
+    cin>>n;
+    for(double i=0;i<=n;i++)
 {
-    int x;
-    cout<<"introduce un numero_"<<endl;
-    cin>>x;
-    if(x<=5)
-    {
-        if(x%3==0)
-        {
-            x=x+2;
-            cout<<"tu numero es_"<<x<<endl;
-        }
-        else
-        {
-            cout<<"tu numero es_"<<x<<endl;
-        }
-    }
-
+    sum=((b-a)/n)*(sin(i))+sum;
+}
+cout<<"el area bajo la curva numerica es_"<<sum<<endl;
+cout<<"el area bajo la curva directa es_"<<cos(b)-cos(a)<<endl;
 return 0;
 }
 
